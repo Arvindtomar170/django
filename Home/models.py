8a07395bf15d713d9a575f47435a81f048b2b896
ARVIND TOMAR <arvindbhut170@gmail.com>
2022-01-13 15:07:34 +0530

    no comment

--- Home/models.py
+++ Home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    id=models.IntegerField(primary_key=True,auto_created=True)
+    # id=models.IntegerField(primary_key=True,auto_created=True)
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
     email=models.CharField(max_length=50,unique=True)
